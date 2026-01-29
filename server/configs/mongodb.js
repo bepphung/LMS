c3fe10436315966cf6f6ff662cf8ee73f0110a76
BackToTheGoodOldDays <backchill17@gmail.com>
2026-01-30 00:31:03 +0700

    Update log to fix mongo

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,10 +1,16 @@
 import mongoose from "mongoose"
 
 // Connect to MongoDB
-
 const connectDB = async () => {
-  mongoose.connection.on('connected', () => console.log('MongoDB connected successfully'))
-  await mongoose.connect(`${process.env.MONGODB_URI}/lms`)
+  console.log('connectDB function called');
+  mongoose.connection.on('connected', () => console.log('MongoDB connected successfully'));
+  mongoose.connection.on('error', (err) => console.error('MongoDB connection error:', err));
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/lms`);
+    console.log('Mongoose connect called with URI:', `${process.env.MONGODB_URI}/lms`);
+  } catch (err) {
+    console.error('MongoDB initial connection error:', err);
+  }
 }
 
 export default connectDB
--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -2,18 +2,20 @@ import { Webhook } from "svix"
 import User from "../models/User.js"
 
 // API Controller function to manage clerk user with database
-
- export const clerkWebhooks = async (req, res) => {
+export const clerkWebhooks = async (req, res) => {
+  console.log('Webhook called');
   try {
-    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
     await whook.verify(JSON.stringify(req.body), {
       'svix-id': req.headers['svix-id'],
       'svix-timestamp': req.headers['svix-timestamp'],
       'svix-signature': req.headers['svix-signature']
-    })
+    });
 
-    const {data, type} = req.body
+    const {data, type} = req.body;
+    console.log('Webhook type:', type);
+    console.log('Webhook data:', data);
 
     switch (type) {
       case 'user.created': {
@@ -22,32 +24,39 @@ import User from "../models/User.js"
           email: data.email_addresses[0].email_address,
           name: data.first_name + ' ' + data.last_name,
           imageUrl: data.image_url
-        }
-        await User.create(userData)
-        res.json({})
-        break
+        };
+        console.log('Preparing to create user:', userData);
+        const user = await User.create(userData);
+        console.log('User created:', user);
+        res.json({});
+        break;
       }
       case 'user.updated': {
         const userData = {
           email: data.email_addresses[0].email_address,
           name: data.first_name + ' ' + data.last_name,
           imageUrl: data.image_url
-        }
-        await User.findByIdAndUpdate(data.id, userData)
-        res.json({})
-        break
+        };
+        console.log('Preparing to update user:', userData);
+        const user = await User.findByIdAndUpdate(data.id, userData);
+        console.log('User updated:', user);
+        res.json({});
+        break;
       }
       case 'user.deleted': {
-        await User.findByIdAndDelete(data.id)
-        res.json({})
-        break
+        console.log('Preparing to delete user with id:', data.id);
+        const user = await User.findByIdAndDelete(data.id);
+        console.log('User deleted:', user);
+        res.json({});
+        break;
       }
-
       default:
-        break
+        console.log('Unhandled webhook type:', type);
+        break;
     }
   } catch (error) {
-    res.json({success: false, message: error.message})
+    console.error('Webhook error:', error);
+    res.json({success: false, message: error.message});
   }
 }
 
