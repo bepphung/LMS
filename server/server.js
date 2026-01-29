ffefc4f4ba75edf3997124a95835f1e1d8f47df5
BackToTheGoodOldDays <backchill17@gmail.com>
2026-01-29 21:29:55 +0700

    LMS from GreatStack youtube channel

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { connect } from 'mongoose'
+import connectDB from './configs/mongodb.js'
+import { clerkWebhooks } from './controllers/webhooks.js'
+
+// Initialize Express app
+const app = express()
+
+// Connect to Database
+await connectDB()
+
+// Middleware
+app.use(cors())
+
+// Routes
+app.get('/', (req, res) => res.send('API Working'))
+app.post('/clerk', express.json(), clerkWebhooks)
+
+// Port
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
+
